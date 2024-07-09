00141b0fd7e92a5367ddb77cc0b3ee7e9a23e51d
mmao rog <d44066041@gs.ncku.edu.tw>
2024-07-10 06:50:58 +0800

    update: creature

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { TezosToolkit } from "@taquito/taquito";
+import "./App.css";
+import ConnectButton from "./components/ConnectButton";
+// import Incubator from "./components/Incubator";
+import From from "./components/Form";
+import { BeaconWallet } from "@taquito/beacon-wallet";
+import { useSkin } from "./hooks/useSkin";
+import { Sketch } from "./components/Sketch";
+const App = () => {
+  const [Tezos] = useState<TezosToolkit>(
+    new TezosToolkit("https://ghostnet.ecadinfra.com")
+  );
+  const [wallet, setWallet] = useState<BeaconWallet | undefined>(undefined);
+  const [userAddress, setUserAddress] = useState<string | undefined>(undefined);
+  const [skin, setSkin] = useSkin(Tezos);
+  const [testSkin, _setTestSkin] = useState({v1:0,v2:0,v3:0,v4:0});
+  const [incubating, setIncubating] = useState(false);
+  const setTestSkin = (skin:string)=>_setTestSkin({v1:parseInt(skin[0]),v2:parseInt(skin[1]),v3:parseInt(skin[2]),v4:parseInt(skin[3])});
+
+  const connectButton = <ConnectButton
+    Tezos={Tezos}
+    userAddress={userAddress}
+    setUserAddress={setUserAddress}
+    setWallet={setWallet}
+    wallet={wallet}
+  />;
+  const sketchOrForm = ()=>{
+    if (incubating){
+      return userAddress ? <From setSkin={setSkin} setIncubating={setIncubating}/> : <From setSkin={setTestSkin} setIncubating={setIncubating}/>;
+    }else{
+      return userAddress ? <Sketch skin={skin}/> : <Sketch skin={testSkin}/>;
+    }
+    
+  }
+  // const incubator = <Incubator setSkin={setSkin} />;
+  const app = <>
+    <div className="Incubator">
+    {connectButton}
+    <button type="button" onClick={()=>setIncubating(true)}>{userAddress ?`   孵化  `:`體驗孵化`}</button>
+    </div>
+    {sketchOrForm()}
+  </>;
+  return app;
+};
+
+export default App;
---
+++ src/hooks/useSkin.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import { TezosToolkit } from "@taquito/taquito";
+import { Skin } from "./useCreature";
+type skinSettor = (skin:string)=>any;
+type useSkinOutput = [Skin, skinSettor];
+
+const env = "KT1C8eVAxajftvXLHTV6nQUz2Xmyh9HbV589";
+// await (async()=>{
+//     const response = await fetch("/env.json");
+//     const file:{Creature:string} = await response.json();
+//     console.log(file.Creature);
+//     return file.Creature;
+// })();
+
+const useSkin = (
+    Tezos: TezosToolkit
+):useSkinOutput => {
+    const [Creature] = useState<string>(env);
+    const [skin, _setSkin] = useState<Skin>({v1:0,v2:0,v3:0,v4:0});
+
+    const updateSkin = async () => {
+        try{
+            const contract = await Tezos.contract.at(Creature);
+            const skin_op:string = await contract.contractViews.get_skin().executeView({ viewCaller: Creature });
+            console.info(skin_op);
+            if(parseInt(skin_op[0]) >= 0){
+                const newSkin = {v1:parseInt(skin_op[0]),v2:parseInt(skin_op[1]),v3:parseInt(skin_op[2]),v4:parseInt(skin_op[3])};
+                console.info(newSkin);
+                _setSkin(newSkin);
+            }
+        }
+        catch{}
+        finally{}
+    };
+
+    const setSkin = async (skin: string) => {
+        try{
+            const contract = await Tezos.wallet.at(Creature);
+            const incubate_op = await contract.methodsObject.incubate(skin).send();
+            await incubate_op.confirmation();
+        }
+        catch{}
+        finally {await updateSkin();}
+    };
+    useEffect(() => {
+        updateSkin();
+    }, []);
+    return [skin, setSkin];
+};
+export { useSkin }
